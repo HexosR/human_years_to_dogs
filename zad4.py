ba75aa2b2f5349eeca61dc84be5f127707c3ed0d
Robert Młynarczyk <48297993+HexosR@users.noreply.github.com>
2019-10-30 00:01:55 +0000

    Add files via upload

---
+++ zad4.py
@@ -0,0 +1,4 @@
+def human_to_dog_years(years = int(input("Input human years that you want to change to the dog years"))):
+	dog_age_in_dog_years = (years - 2) * 4 + 2*10.5   
+	print (dog_age_in_dog_years)
+
